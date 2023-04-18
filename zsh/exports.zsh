f947925449597d4a8e04cee044bf04ef2459d70d
gongyibei <gongyibei@gmail.com>
2023-04-18 11:49:27 +0800

    change exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,11 +1,14 @@
 # my personal script
-export PATH=~/.dotfiles/bin/:$PATH
-export PATH=~/.iterm2/:$PATH
+export PATH=~/.dotfiles/bin:$PATH
+export PATH=~/.iterm2:$PATH
 export PATH=.:$PATH
 export PATH=/usr/local/go/bin:$PATH
+export PATH=/opt/homebrew/bin:$PATH
+
+export DYLD_LIBRARY_PATH=/opt/homebrew/lib:$DYLD_LIBRARY_PATH
 
 # homebrew mirror
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
 # google application
 export GOOGLE_APPLICATION_CREDENTIALS=/Users/swdy/Applications/google-cloud-sdk/ath.json
